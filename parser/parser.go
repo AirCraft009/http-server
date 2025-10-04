61f67c5abb26cfebf8239c861a40b317d4d149f4
AirCraft009 <coconraupe@gmail.com>
2025-10-04 20:06:53 +0200

    added accepting connections and basic handling without response

---
+++ parser/parser.go
@@ -0,0 +1,8 @@
+package parser
+
+type Parser struct {
+}
+
+func NewParser() *Parser {
+	return &Parser{}
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,66 @@
 package server
+
+import (
+	"fmt"
+	"http-server/parser"
+	"net"
+	"strconv"
+)
+
+// Server
+// supports tcp connections/*
+type Server struct {
+	port     int
+	listener net.Listener
+	parser   *parser.Parser
+}
+
+func checkErorr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewServer(port int) *Server {
+	return &Server{port, net.Listener(nil), parser.NewParser()}
+}
+
+func (s *Server) Listen() {
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(s.port))
+	checkErorr(err)
+	s.listener = listener
+}
+
+func (s *Server) AcceptConnections() {
+	if s.listener == nil {
+		s.Listen()
+	}
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			fmt.Printf("Error accepting conn: %s\n", err.Error())
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	//min size for header 20B max 60B
+	header := make([]byte, 60)
+	//n is the ammount of bytes that are relevant so only header[n:] is important!!
+	n, err := conn.Read(header)
+	if err != nil {
+		fmt.Printf("Error reading header: %s\nerr: %s\n", conn, err.Error())
+	}
+	//read everything up to n (inklusive)
+	fmt.Println(string(header[:n]))
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		checkErorr(err)
+	}(conn)
+}
+
+func (s *Server) Close() {
+	err := s.listener.Close()
+	checkErorr(err)
+}
